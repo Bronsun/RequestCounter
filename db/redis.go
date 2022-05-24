dde25436006ffe4e9d2edb81800a161df08d04f6
Bronsun <mateusz.broncel1998@gmail.com>
2022-05-24 17:15:53 +0200

    Fixed linter in RedisConnect function

--- db/redis.go
+++ db/redis.go
@@ -7,16 +7,17 @@ import (
 	"github.com/go-redis/redis"
 )
 
-var client *redis.Client
+var Client *redis.Client
 
+// RedisConnect connect to Redis database
 func RedisConnect() redis.Client {
-	client := redis.NewClient(&redis.Options{
+	Client = redis.NewClient(&redis.Options{
 		Addr:     os.Getenv("REDIS_DB_ADDR"),
 		Password: os.Getenv("REDIS_DB_PASSWORD"),
 		DB:       0,
 	})
-	pong, err := client.Ping().Result()
+	pong, err := Client.Ping().Result()
 	fmt.Println(pong, err)
 
-	return *client
+	return *Client
 }
